1c3c71f475810be730947a335fa02ad8d9b5626e
Andres Rey <andreskrey@gmail.com>
2019-10-07 21:31:04 +0100

    Add base tests

---
+++ tests/DOMDocumentComparatorTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace andreskrey\Tests\PHPUnit;
+
+use andreskrey\PHPUnit\Comparator\NodeComparator;
+use andreskrey\PHPUnit\DOMDocumentComparator;
+use PHPUnit\Framework\TestCase;
+
+class DOMDocumentComparatorTest extends TestCase
+{
+    use FilesDataProviderTrait;
+
+    /**
+     * @dataProvider equalDocumentsDataProvider
+     * @param string $original
+     * @param string $other
+     */
+    public function testDOMDocumentsAreEqual(string $original, string $other)
+    {
+        $originalDOM = new \DOMDocument();
+        $originalDOM->loadHTML($original);
+        $otherDOM = new \DOMDocument();
+        $otherDOM->loadHTML($other);
+
+        $constraint = new DOMDocumentComparator($originalDOM, new NodeComparator());
+
+        $this->assertTrue($constraint->matches($otherDOM));
+    }
+
+    public function testDOMDocumentsAreNotEqual()
+    {
+
+    }
+}
---
+++ tests/FilesDataProviderTrait.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace andreskrey\Tests\PHPUnit;
+
+trait FilesDataProviderTrait
+{
+    public function equalDocumentsDataProvider()
+    {
+        $path = pathinfo(__FILE__, PATHINFO_DIRNAME) . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'EqualDocuments';
+        $directories = array_slice(scandir($path), 2);
+
+        yield from $this->getFiles($path, $directories);
+    }
+
+    public function nonEqualDocumentsDataProvider()
+    {
+        $path = pathinfo(__FILE__, PATHINFO_DIRNAME) . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'NonEqualDocuments';
+        $directories = array_slice(scandir($path), 2);
+
+        yield from $this->getFiles($path, $directories);
+    }
+
+    private function getFiles(string $path, array $testDirectories): \Generator
+    {
+        foreach ($testDirectories as $testPage) {
+            $testCasePath = $path . DIRECTORY_SEPARATOR . $testPage . DIRECTORY_SEPARATOR;
+
+            $original = file_get_contents($testCasePath . 'original.html');
+            $other = file_get_contents($testCasePath . 'other.html');
+
+            yield $testPage => [$original, $other];
+        }
+    }
+}
